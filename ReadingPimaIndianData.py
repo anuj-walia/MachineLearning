824373cd6b9662c820523c7d227cc14db4e8dfc9
anuj-walia <anuj_walia@protonmail.com>
2020-01-19 22:36:04 -0500

    Added code for molding data module

--- ReadingPimaIndianData.py
+++ ReadingPimaIndianData.py
@@ -21,10 +21,37 @@ def plot_corr(dataframe, size=11):
     ax.matshow(corr) #color code the correlations by the correlation value
     plt.xticks(range(len(corr.columns)),corr.columns) # draw x tickmarks
     plt.yticks(range(len(corr.columns)),corr.columns) # draw y tickmarks
-    plt.show()
+    # plt.show()
 
 
 df = pd.read_csv("/tmp/ml/data/pima-data.csv")
 print(df.shape)
 print(df.isnull().values.any())
 plot_corr(df)
+print(df.head(10))
+# print(df['skin','thickness'])
+print("=================skin column=================")
+print(df['skin'])
+print("=================skin column end=================")
+del df['skin']
+print("==============printing first 10 rows====================")
+print(df.head(10))
+print("================printing first 10 rows end========================")
+print("=================printing shape=================")
+print(df.shape)
+print("=================printing shape end=================")
+plot_corr(df)
+plot_corr(df,10)
+
+boolean_map={True:1,False:0}
+boolean_map_String={'True':1,'False':0}
+df['diabetes']=df['diabetes'].map(boolean_map)
+
+print("==============printing first 10 rows====================")
+print(df.head(10))
+print("================printing first 10 rows end========================")
+
+num_true=len(df.loc[df['diabetes']==1])
+num_false=len(df.loc[df['diabetes']==0])
+print('Number of true cases: {0} {1:2.2f}'.format(num_true,(num_true/(num_false+num_true))*100))
+print('Number of false cases: {0} {1:2.2f}'.format(num_false,(num_false/(num_false+num_true))*100))
